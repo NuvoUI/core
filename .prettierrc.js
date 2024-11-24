86997bf1800cbf19eb064af0e8e9526ac8d6e988
Aamir <aamir@aalasolutions.com>
2024-11-24 14:46:03 +0400

    prettier and stylint implemented

---
+++ .prettierrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  overrides: [
+    {
+      files: '*.scss',
+      options: {
+        singleQuote: false,
+        printWidth: 120,
+        bracketSpacing: false,
+        singleLineBrackets: true
+      }
+    }
+  ]
+};
